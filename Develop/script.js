bbc946b480e1a274a4590f34839f73899ae0d8bf
Jon <jonathancerruti@gmail.com>
2022-06-26 09:54:20 -0700

    save button storing to console time blocks still mot changing color

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,58 @@
-// display date and time inside the jumbotron
+// display date inside the jumbotron
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 
-let renderDateTime = function(){
-    document.querySelector("#currentDay").innerHTML =
-    `
-    <h5> ${moment().format('MMMM Do YYYY, h:mm:ss a')}<h5>
-    `;
+//variables
+// let timeBlock = document.querySelector('.timeBlock');
+// let time = document.querySelector('.time');
+
+var saveBtn = $(".saveBtn");
+
+//color time blocks based on hour of the day past = gray present = red futire = green
+function renderHourColor(){
+   var time = moment().hours();
+
+   $(".time-block").each(function () {
+            var hourBlock = parseInt($(this).attr("id"));
+            // console.log( hourBlock);
+            // console.log(this);
+
+            if (hourBlock > time) {
+                $(this).addClass(".future");
+                // console.log(this);
+            }
+            else if (hourBlock === time){
+                $(this).addClass(".present");
+            }
+
+            else {
+                $(this).addClass(".past");
+            }
+
+    })
+
 };
 
+saveBtn.on("click", function() {
 
-renderDateTime();
-setInterval(renderDateTime, 1000);
+    // console.log(this); //save button
+    var hour = $(this).siblings(".time").text();
+    var task = $(this).siblings(".task").val();
 
-//
+    // THEN the text for that event is saved in local storage
+    localStorage.setItem(hour, task);
+});
+
+function workDay(){
+    $(".time").each(function(){
+        var hourBlock = $(this).text();
+        var currentTask = localStorage.getItem(hourBlock);
+
+        if (currentTask!== null){
+            $(this).siblings(".task").val(currentTask);
+        }
+    });
+}
+
+renderHourColor();
+console.log(renderHourColor);
+workDay();
